57f3ef344df38703d37095808305fb771ae7fb14
Jake Topping <email@jaketopping.co.uk>
2021-10-04 13:28:15 +0100

    Added search scripts

---
+++ 20211003_search_all.py
@@ -0,0 +1,272 @@
+import ast
+from multiprocessing import Pool
+import numpy as np
+import pickle as pkl
+import seaborn as sns
+import sherpa
+import time
+
+def get_parameters(dataset, preprocessing, output_dir, **kwargs):
+    parameters = [
+        sherpa.Choice(name='dataset', range=[dataset]),
+        sherpa.Choice(name='preprocessing', range=[(preprocessing if not preprocessing.startswith('sdrfc') else 'sdrfc') if not preprocessing.startswith('sdrfcu') else 'sdrfcu']),
+        sherpa.Choice(name='output_dir', range=[output_dir]),
+        sherpa.Choice(name='num_development', range=[kwargs['num_development']]),
+        sherpa.Choice(name='device', range=['cuda']),
+        sherpa.Discrete(name='hidden_layers', range=[1,2]), # [1,3]
+        sherpa.Ordinal(name='hidden_units', range=[16,32,64,128]),
+        sherpa.Continous(name='dropout', range=[0.2,0.8]),
+        sherpa.Continous(name='lr', range=[0.005,0.05], scale='log'),
+        sherpa.Continous(name='weight_decay', range=[0.01,0.2], scale='log'),
+    ]
+    if (preprocessing == 'none') or (preprocessing == 'undirected'):
+        pass
+    elif (preprocessing == 'ppr') or (preprocessing == 'undirected_ppr'):
+        parameters += [
+            sherpa.Continuous(name='alpha', range=[0.01, 0.12], scale='log'),
+            sherpa.Ordinal(name='k', range=[16,32,64]),
+            sherpa.Continous(name='epsilon', range=[0.0001, 0.001], scale='log'),
+            sherpa.Choice(name='use_k', range=[True, False]),
+        ]
+    elif (preprocessing == 'sdrfct') or (preprocessing == 'sdrfcut'):
+        parameters += [
+            sherpa.Discrete(name='max_steps', range=kwargs['max_steps_range']),
+            sherpa.Choice(name='remove_edges', range=[True]),
+            sherpa.Discrete(name='tau', range=kwargs['tau_range'], scale='log'),
+        ]
+    elif (preprocessing == 'sdrfcf') or (preprocessing == 'sdrfcuf'):
+        parameters += [
+            sherpa.Discrete(name='max_steps', range=kwargs['max_steps_range']),
+            sherpa.Choice(name='remove_edges', range=[False]),
+            sherpa.Discrete(name='tau', range=kwargs['tau_range'], scale='log'),
+        ]
+    return parameters
+
+
+class Experiment:
+    def __init__(self, dataset, preprocessing, search_script, max_num_trials=50, sleep_t=0, **kwargs):
+        self.dataset = dataset
+        self.preprocessing = preprocessing
+        self.search_script = search_script
+        self.max_num_trials = max_num_trials
+        self.sleep_t = sleep_t
+        self.kwargs = kwargs
+
+def gen_experiments():
+    experiments = []
+    t = 0
+
+    # WebKB experiments
+    for dataset in ['Cornell', 'Wisconsin', 'Texas']:
+        for preprocessing in ['none', 'undirected', 'ppr', 'undirected_ppr']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    num_development=0,
+                    hours=1,
+                    max_num_trials=40,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+        for preprocessing in ['sdrfct', 'sdrfcf', 'sdrfcut', 'sdrfcuf']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    tau_range=[50,200],
+                    max_steps_range=[10,1000],
+                    num_development=0,
+                    hours=1,
+                    max_num_trials=100,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+
+    # DIGL experiments #1
+    for dataset in ['Cora', 'Citeseer']:
+        for preprocessing in ['none', 'undirected', 'ppr', 'undirected_ppr']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    num_development=1500,
+                    hours=1,
+                    max_num_trials=40,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+        for preprocessing in ['sdrfct', 'sdrfcf', 'sdrfcut', 'sdrfcuf']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    tau_range=[50,400],
+                    max_steps_range=[300,3000],
+                    num_development=1500,
+                    hours=2,
+                    max_num_trials=100,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+
+    # DIGL experiments #2
+    for dataset in ['Pubmed']:
+        for preprocessing in ['none', 'undirected', 'ppr', 'undirected_ppr']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    num_development=1500,
+                    hours=1,
+                    max_num_trials=40,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+        for preprocessing in ['sdrfct', 'sdrfcf', 'sdrfcut', 'sdrfcuf']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    tau_range=[50,400],
+                    max_steps_range=[300,3000],
+                    num_development=1500,
+                    hours=3,
+                    max_num_trials=100,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+
+    # Wikipedia experiments #1
+    for dataset in ['Chameleon']:
+        for preprocessing in ['none', 'undirected', 'ppr', 'undirected_ppr']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    num_development=600,
+                    hours=1,
+                    max_num_trials=40,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+        for preprocessing in ['sdrfct', 'sdrfcf', 'sdrfcut', 'sdrfcuf']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    tau_range=[30,400],
+                    max_steps_range=[200,2500],
+                    num_development=600,
+                    hours=2,
+                    max_num_trials=100,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+
+    # Wikipedia experiments #2
+    for dataset in ['Squirrel', 'Actor']:
+        for preprocessing in ['none', 'undirected', 'ppr', 'undirected_ppr']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    num_development=1500,
+                    hours=1,
+                    max_num_trials=40,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+        for preprocessing in ['sdrfct', 'sdrfcf', 'sdrfcut', 'sdrfcuf']:
+            experiments.append(
+                Experiment(
+                    dataset,
+                    preprocessing,
+                    '20211003_search_experiment.py',
+                    tau_range=[50,400],
+                    max_steps_range=[200,3000],
+                    num_development=1500,
+                    hours=3,
+                    max_num_trials=100,
+                    sleep_t=t,
+                )
+            )
+            t += 15
+
+    return experiments
+
+experiments = gen_experiments()
+
+
+def run_experiment(experiment, max_num_trials=100, max_concurrent=None):
+    time.sleep(experiment.sleep_t)
+    experiment_name = experiment.dataset[:4]+'_'+experiment.preprocessing
+    max_num_trials = experiment.max_num_trials
+    if max_concurrent is None:
+        max_concurrent = max_num_trials
+    output_dir = '/boffice/ots/users/jtopping/sherpa_final/'+experiment_name
+    parameters = get_parameters(experiment.dataset, experiment.preprocessing, output_dir, **experiment.kwargs)
+    algorithm = sherpa.algorithms.RandomSearch(max_num_trials=max_num_trials)
+    scheduler = sherpa.schedulers.SGEScheduler(submit_options=f'-N {experiment_name} -l h_rt={experiment.kwargs["hours"]}:0:0,h_vmem=8G,estmem=8G,gpu_slots=1 -j y -S /bin/bash',
+        environment='/home/jtopping/.julia/conda/3/bin/activate && conda activate env_clr && module load cuda-10.1.243')
+
+    results = sherpa.optimize(
+        parameters=parameters,
+        algorithm=algorithm,
+        lower_is_better=False,
+        filename=f'/home/jtopping/dev/working/gbcbr/notebooks/{experiment.search_script}',
+        scheduler=scheduler,
+        verbose=0,
+        max_concurrent=max_concurrent,
+        output_dir=output_dir,
+        disable_dashboard=True
+    )
+
+    best_trial_id = results['Trial-ID']
+    with open(f'{output_dir}/jobs/trial_{best_trial_id}.out', 'r+') as f:
+        lines = f.readlines()
+        for l in lines[::-1]:
+            if l.startswith('Here!'):
+                test_accs = ast.literal_eval(l[5:])['test_acc']
+                break
+        boots_series = sns.algorithms.bootstrap(test_accs, func=np.mean, n_boot=1000)
+        test_acc_mean = np.mean(test_accs)
+        test_acc_ci = np.max(np.abs(sns.utils.ci(boots_series, 95) - test_acc_mean))
+
+    pkl.dump(
+        (experiment_name, test_acc_mean, test_acc_ci, results),
+        open(
+            output_dir+'/best_trial.pkl',
+            'wb'
+        )
+    )
+
+    return experiment_name, test_acc_mean, test_acc_ci
+
+with Pool(len(experiments)) as p:
+    results = p.map(run_experiment, experiments)
+pkl.dump(
+    results,
+    open(
+        '/boffice/ots/users/jtopping/sherpa_final/results_1.pkl',
+        'wb'
+    )
+)
---
+++ 20211003_search_experiment.py
@@ -0,0 +1,181 @@
+import time
+import yaml
+import torch
+
+import scipy.sparse as sp
+import numpy as np
+import seaborn as sns
+import torch.nn.functional as F
+
+from tqdm.notebook import tqdm
+from torch.optim import Adam, Optimizer
+from collections import defaultdict
+from torch_geometric.data import Data, InMemoryDataset
+
+from digl.data import get_dataset, PPRDataset, SDRFCDataset, UndirectedDataset, UndirectedPPRDataset, set_train_val_test_split, set_train_val_test_split_webkb
+from digl.models import GCN
+from digl.seeds import val_seeds, test_seeds
+
+import sherpa
+
+client = sherpa.Client()
+trial = client.get_trial()
+
+DATASET = trial.parameters['dataset']
+USE_LCC = True
+DEVICE = trial.parameters['device']
+PREPROCESSING = trial.parameters['preprocessing']
+
+if PREPROCESSING == 'none':
+    dataset = get_dataset(
+        name=DATASET,
+        use_lcc=USE_LCC,
+    )
+if PREPROCESSING == 'undirected':
+    dataset = UndirectedDataset(
+        name=DATASET,
+        use_lcc=USE_LCC,
+    )
+elif PREPROCESSING == 'ppr':
+    dataset = PPRDataset(
+        name=DATASET,
+        use_lcc=USE_LCC,
+        alpha=trial.parameters['alpha'],
+        k=trial.parameters['k'] if trial.parameters['use_k'] else None,
+        eps=trial.parameters['eps'] if not trial.parameters['use_k'] else None,
+    )
+elif PREPROCESSING == 'undirected_ppr':
+    dataset = UndirectedPPRDataset(
+        name=DATASET,
+        use_lcc=USE_LCC,
+        alpha=trial.parameters['alpha'],
+        k=trial.parameters['k'] if trial.parameters['use_k'] else None,
+        eps=trial.parameters['eps'] if not trial.parameters['use_k'] else None,
+    )
+elif PREPROCESSING == 'sdrfc':
+    dataset = SDRFCDataset(
+        name=DATASET,
+        use_lcc=USE_LCC,
+        max_steps=trial.parameters['max_steps'],
+        remove_edges=trial.parameters['remove_edges'],
+        tau=trial.parameters['tau'],
+        is_undirected=False
+    )
+elif PREPROCESSING == 'sdrfcu':
+    dataset = SDRFCDataset(
+        name=DATASET,
+        use_lcc=USE_LCC,
+        max_steps=trial.parameters['max_steps'],
+        remove_edges=trial.parameters['remove_edges'],
+        tau=trial.parameters['tau'],
+        is_undirected=True
+    )
+dataset.data = dataset.data.to(DEVICE)
+
+model = GCN(
+    dataset,
+    hidden=trial.parameters['hidden_layers'] * [trial.parameters['hidden_units']],
+    dropout=trial.parameters['dropout']
+).to(DEVICE)
+
+def train(model: torch.nn.Module, optimizer: Optimizer, data: Data):
+    model.train()
+    optimizer.zero_grad()
+    logits = model(data)
+    loss = F.nll_loss(logits[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+def evaluate(model: torch.nn.Module, data: Data, test: bool):
+    model.eval()
+    with torch.no_grad():
+        logits = model(data)
+    eval_dict = {}
+    keys = ['val', 'test'] if test else ['val']
+    for key in keys:
+        mask = data[f'{key}_mask']
+        # loss = F.nll_loss(logits[mask], data.y[mask]).item()
+        # eval_dict[f'{key}_loss'] = loss
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+        eval_dict[f'{key}_acc'] = acc
+    return eval_dict
+
+
+def run(dataset: InMemoryDataset,
+        model: torch.nn.Module,
+        seeds: np.ndarray,
+        test: bool = False,
+        max_epochs: int = 10000,
+        patience: int = 100,
+        lr: float = 0.01,
+        weight_decay: float = 0.01,
+        num_development: int = 1500,
+        device: str = 'cuda'):
+    start_time = time.perf_counter()
+
+    best_dict = defaultdict(list)
+
+    cnt = 0
+    for seed in tqdm(seeds):
+        if num_development > 0:
+            dataset.data = set_train_val_test_split(
+                seed,
+                dataset.data,
+                num_development=num_development,
+            ).to(device)
+        else:
+            dataset.data = set_train_val_test_split_webkb(
+                seed,
+                dataset.data,
+                num_development=int(dataset.data.y.shape[0]*0.5),
+                train_proportion=0.66,
+            ).to(device)
+        model.to(device).reset_parameters()
+        optimizer = Adam(
+            [
+                {'params': model.non_reg_params, 'weight_decay': 0},
+                {'params': model.reg_params, 'weight_decay': weight_decay}
+            ],
+            lr=lr
+        )
+
+        patience_counter = 0
+        tmp_dict = {'val_acc': 0}
+
+        for epoch in range(1, max_epochs + 1):
+            if patience_counter == patience:
+                break
+
+            train(model, optimizer, dataset.data)
+            eval_dict = evaluate(model, dataset.data, test)
+
+            if eval_dict['val_acc'] < tmp_dict['val_acc']:
+                patience_counter += 1
+            else:
+                patience_counter = 0
+                tmp_dict['epoch'] = epoch
+                for k, v in eval_dict.items():
+                    tmp_dict[k] = v
+
+        for k, v in tmp_dict.items():
+            best_dict[k].append(v)
+
+    client.send_metrics(trial=trial, iteration=epoch, objective=np.mean(best_dict['val_acc']))
+            
+    best_dict['duration'] = time.perf_counter() - start_time
+    return dict(best_dict)
+
+best_dict = run(
+    dataset,
+    model,
+    seeds=test_seeds,
+    lr=trial.parameters['lr'],
+    weight_decay=trial.parameters['weight_decay'],
+    test=True,
+    num_development=trial.parameters['num_development'],
+    device=DEVICE,
+    patience=25
+)
+
+print(f"Here!{best_dict}")
