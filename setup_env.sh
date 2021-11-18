739a32fec710bb5969ebd7668868c3337f503e79
Ben Chamberlain <bchamberlain@twitter.com>
2021-11-18 13:05:29 +0000

    cuda version

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda create -n env_gdl python=3.6 numpy pandas networkx matplotlib seaborn jupyterlab --yes
 conda activate env_gdl
 
-conda install pytorch=1.9 torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia --yes
+conda install pytorch=1.9 torchvision torchaudio cudatoolkit=10.2 -c pytorch -c nvidia --yes
 conda install pyg -c pyg -c conda-forge --yes
 
 pip install -U ray[tune]
